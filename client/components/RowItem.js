9476b33cf4987f23b748fb3e8f93b7aef08ca40e
Callum McLuskey <callummcluskey100@gmail.com>
2022-11-29 22:25:59 +0000

    added profile page

--- client/components/RowItem.js
+++ client/components/RowItem.js
@@ -1,34 +1,60 @@
 import { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { TouchableOpacity, Text, View } from "react-native";
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from "react-native-vector-icons/Feather";
 
-class RowItem extends Component{
-    render(){
-        const {title, icon, colorBG: backgroundColor} = this.props;
-        return(
-            <View style={{paddingTop:5, alignItems:"center"}}>
-                <TouchableOpacity style={{backgroundColor, width:350, height:60, paddingTop:10, borderRadius:16, marginHorizontal:1.5, borderColor:"black", borderWidth:2 }}>
-                    <View style={{flexDirection:"row"}}>
-                        <View style={{marginLeft:5}}>
-                            <Icon name={icon} size={40} color={"black"}/>
-                        </View>
-                        <Text style={{color:"black", fontSize:25, textAlign:"center", marginLeft:10}}>{title}</Text>
-                        <View style={{marginLeft:"auto", marginRight:5}}>
-                            <Icon name="arrow-right" size={40} color={"black"}/>
-                        </View>
-                    </View>
-                </TouchableOpacity>
+class RowItem extends Component {
+  render() {
+    const { title, icon, colorBG: backgroundColor, style } = this.props;
+    return (
+      <View
+        style={
+          style
+            ? { paddingTop: 5, alignItems: "center", ...style }
+            : { paddingTop: 5, alignItems: "center" }
+        }
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor,
+            width: 350,
+            height: 60,
+            paddingTop: 10,
+            borderRadius: 16,
+            marginHorizontal: 1.5,
+            borderColor: "black",
+            borderWidth: 2,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={{ marginLeft: 5 }}>
+              <Icon name={icon} size={40} color={"black"} />
             </View>
-        );
-
-    }
+            <Text
+              style={{
+                width: 230,
+                color: "black",
+                fontSize: 25,
+                textAlign: "center",
+                marginLeft: 10,
+              }}
+            >
+              {title}
+            </Text>
+            <View style={{ marginLeft: "auto", marginRight: 5 }}>
+              <Icon name="arrow-right" size={40} color={"black"} />
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
-RowItem.propTypes={
-    title: PropTypes.string,
-    icon: PropTypes.string,
-    colorBG: PropTypes.string
+RowItem.propTypes = {
+  title: PropTypes.string,
+  icon: PropTypes.string,
+  colorBG: PropTypes.string,
 };
 
 export default RowItem;
--- client/views/profile.js
+++ client/views/profile.js
@@ -1,7 +1,79 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
+import RowItem from "../components/RowItem";
 
-export const Profile = () => (
-  <View style={{ padding: 10, paddingTop: 60 }}>
-    <Text>Profile Page</Text>
-  </View>
-);
+export const Profile = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/background.png")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Image
+          source={require("../assets/logo.png")}
+          style={{
+            width: 250,
+            height: 75,
+            marginRight: "auto",
+            marginLeft: 10,
+          }}
+        />
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 300,
+            alignItems: "center",
+            margin: 30,
+          }}
+        >
+          <Text style={{ fontSize: 40, fontWeight: "bold" }}>Hey Callum!</Text>
+          <View style={{ display: "flex", alignItems: "center" }}>
+            <Text>Today</Text>
+            <Text style={{ fontSize: 50 }}>😀</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: 320,
+            height: 250,
+            backgroundColor: "white",
+            borderRadius: 20,
+          }}
+        ></View>
+        <RowItem
+          style={{ marginTop: 40 }}
+          title="Settings"
+          icon="settings"
+          colorBG="#FFD29C"
+        />
+        <RowItem
+          style={{ marginTop: 20 }}
+          title="View all data"
+          icon="list"
+          colorBG="#FFD29C"
+        />
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+});
